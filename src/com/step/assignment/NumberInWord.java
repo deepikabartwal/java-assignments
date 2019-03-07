72ea02ebc0a4e0587b0664e3fda335fceca3f560
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-03-07 14:05:42 +0530

    edited output of the varius cases

--- src/com/step/assignment/NumberInWord.java
+++ src/com/step/assignment/NumberInWord.java
@@ -1,43 +1,44 @@
 package com.step.assignment;
 
 public class NumberInWord {
-    public static void main(String[] args) {
-        printNumberInWord(0);
-    }
     public static void printNumberInWord(int number){
         System.out.println(convertNumberInWord(number));
     }
     public static String convertNumberInWord(int number){
-        if(number==0){
-            return "";
+        switch(number){
+            case 0: {
+                return "Zero";
+            }
+            case 1: {
+                return "One";
+            }
+            case 2: {
+                return "Two";
+            }
+            case 3: {
+                return "Three";
+            }
+            case 4: {
+                return "Four";
+            }
+            case 5: {
+                return "Five";
+            }
+            case 6: {
+                return "Six";
+            }
+            case 7: {
+                return "Seven";
+            }
+            case 8: {
+                return "Eight";
+            }
+            case 9: {
+                return "Nine";
+            }
+            default: {
+                return "Other";
+            }
         }
-        if(number==1){
-            return "ONE";
-        }
-        if(number==2){
-            return "TWO";
-        }
-        if(number==3){
-            return "THREE";
-        }
-        if(number==4){
-            return "FOUR";
-        }
-        if(number==5){
-            return "FIVE";
-        }
-        if(number==6){
-            return "SIX";
-        }
-        if(number==7){
-            return "SEVEN";
-        }
-        if(number==8){
-            return "EIGHT";
-        }
-        if(number==9){
-            return "NINE";
-        }
-        return "OTHER";
     }
 }
